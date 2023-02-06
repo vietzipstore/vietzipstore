bb8d87a1eb736ff478c517bd5042ec96d6fa1828
HiLu882 <hien.luong.vtc@gmail.com>
2023-02-06 15:51:37 +0700

    add

--- css-js/adsgoogle.js
+++
@@ -1,3 +0,0 @@
-<!--QC google adsen  -->
-<script type="async" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8308471181946591" crossorigin="anonymous"></script>
-<!--QC google adsen  -->
